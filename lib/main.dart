ec3dbd681fa8b42630173b4d7fdbbe2e8938d4cc
Abdi Daraje <adaraje@pop-os.localdomain>
2020-05-16 18:47:08 +0300

    built my 3rd app today that plays different sound on button press

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -6,11 +7,39 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              getAudioPlayButton(Colors.red, 1),
+              getAudioPlayButton(Colors.orange, 2),
+              getAudioPlayButton(Colors.yellow, 3),
+              getAudioPlayButton(Colors.green, 4),
+              getAudioPlayButton(Colors.teal, 5),
+              getAudioPlayButton(Colors.blue, 6),
+              getAudioPlayButton(Colors.purple, 7),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+Widget getAudioPlayButton(Color color, int index) {
+  return Expanded(
+    child: FlatButton(
+      onPressed: () {
+        playAudio(index);
+      },
+      color: color,
+    ),
+  );
+}
+
+void playAudio(int index) {
+  final player = AudioCache();
+  player.play('note$index.wav');
 }
